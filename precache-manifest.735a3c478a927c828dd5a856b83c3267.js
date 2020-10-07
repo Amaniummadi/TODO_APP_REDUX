8e894e4f6f6995ec7743cac5305ab5d6baabf64b
amaniummadi <amaniummadi123@gmail.com>
2020-10-07 08:09:32 +0530

    Updates

---
+++ precache-manifest.735a3c478a927c828dd5a856b83c3267.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddab81256d3a7ecc98b31729f0ff0e01",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "68c47c5f1cc57ca5463c",
+    "url": "/todoapp/static/css/2.220ccb42.chunk.css"
+  },
+  {
+    "revision": "8df8757cc94334968115",
+    "url": "/todoapp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "68c47c5f1cc57ca5463c",
+    "url": "/todoapp/static/js/2.f8585ce8.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/todoapp/static/js/2.f8585ce8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8df8757cc94334968115",
+    "url": "/todoapp/static/js/main.bd332038.chunk.js"
+  },
+  {
+    "revision": "f6dada7933e7c433e779",
+    "url": "/todoapp/static/js/runtime-main.f10bf690.js"
+  }
+]);
